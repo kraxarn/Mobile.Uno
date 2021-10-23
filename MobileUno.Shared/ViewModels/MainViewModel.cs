468788377ba42e821687cd86a51711011c6087ed
kraxarn <kraxie@protonmail.com>
2021-10-23 22:34:35 +0200

    Set from constructor

--- MobileUno.Shared/ViewModels/MainViewModel.cs
+++ MobileUno.Shared/ViewModels/MainViewModel.cs
@@ -10,15 +10,18 @@ public class MainViewModel
 	{
 		var season = DateTime.Now.GetSeason();
 		BackgroundSource = $"Assets/Images/{season.ToString().ToLower()}.jpg";
+
+		CurrentUser = "Sample User";
+		CurrentCompany = "000 Sample Company";
 	}
 
 	public string BackgroundSource { get; }
 
-	public string CurrentUser => "Sample User";
+	public string CurrentUser { get; }
 
 	public string CurrentUserInitials => CurrentUser
 		.Split(' ')
 		.Aggregate(string.Empty, (s, w) => s + char.ToUpper(w[0]));
 
-	public string CurrentCompany => "000 Sample Company";
+	public string CurrentCompany { get; }
 }
